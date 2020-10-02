583d4048b14add993c38fcfe12f721a9a73b9417
Seiba Shonia <cibss13@gmail.com>
2020-10-02 17:51:54 +0700

    main news done

--- components/MainNews.vue
+++ components/MainNews.vue
@@ -1,12 +1,20 @@
 <template>
-    <b-container style="display: flex;">
+    <b-container style="display: flex;" class="container">
         <div class="left">
             <b-img fluid src="~/assets/main-news.JPG" class="mt-5"> </b-img>
-            <h4>Demystifying Palantir: The Most Controversial Company to Go Public in 2020</h4>
+            <h3>Demystifying Palantir: The Most Controversial Company to Go Public in 2020</h3>
             <p>The software and consulting business is doubling down on its work with government agencies</p>
 
-            <div>
-                <b-avatar flex src="https://miro.medium.com/fit/c/80/80/1*aQM0PDcKq_I833QjiifY0A.jpeg"></b-avatar>
+            <div class="author-wrap mt-3">
+                <div>
+                    <b-avatar flex class="avatar" src="https://miro.medium.com/fit/c/80/80/1*aQM0PDcKq_I833QjiifY0A.jpeg"></b-avatar>
+                </div>
+                <div class="ml-3">
+                    <p>
+                        Owen Williams in <a href="https://debugger.medium.com/">Debugger</a> <br>
+                    </p>
+                    <p>Oct 2 · 4 min read</p>
+                </div>
             </div>
 
             <h6 class="mt-5">LATEST</h6>
@@ -30,9 +38,18 @@ export default {
 </script>
 
 <style scoped>
+    h3{
+        font-weight: bold;
+    }
+
+    p{
+        font-size: 14px;
+        margin: 0;
+    }
+
     .left{
         float: left;
-        width: 60%;
+        width: 69%;
         margin-right: 11%;
         justify-content: center;
     }
@@ -42,9 +59,26 @@ export default {
         width: 29%;
     }
 
+    a{
+        color: inherit;
+        font-style: none;
+    }
+
+    .author-wrap {
+        width: 100%;
+        display: flex;
+        align-items: center
+    }
+
+    .avatar {
+        display: block
+    }
+    
+
     @media only screen and (max-width: 1024px) {
         .right{
-            visibility: hidden;
+            /* visibility: hidden; */
+            display: none;
         }
     }
 </style>
--- components/Menu.vue
+++ components/Menu.vue
@@ -1,18 +1,17 @@
 <template>
-    <b-container>
-        <b-navbar class="mt-2">
+    <b-container class="elevated">
+        <b-navbar class="mt-2 ml-5 mr-5">
             <b-navbar-brand href="https://medium.com">
                 <img src="~/assets/medium-logo.png" class="d-inline-block align-top" alt="logo">
             </b-navbar-brand>
 
             <b-navbar-nav>
-                <!-- <b-nav-item style="font-size:20px">|</b-nav-item> -->
+                <hr class="divider">
                 <b-nav-item href="https://medium.com/topic/software-engineering" style="font-size:18px">
                     Software Engineering
                 </b-nav-item>
             </b-navbar-nav>
 
-            <!-- Right aligned nav items -->
             <b-navbar-nav class="ml-auto">
                 <b-nav-item href="https://medium.com/membership" class="right-menu">Become a member</b-nav-item>
                 <b-nav-item href="https://medium.com/m/signin" class="right-menu">Sign in</b-nav-item>
@@ -35,9 +34,25 @@
         font-size: 14px;
     }
 
+    .divider{
+        width: 25px;
+        border-top: 1px solid #8c8b8b;
+        transform: rotate(90deg);
+    }
+
+    /* .elevated { 
+            box-shadow: 1px 2px  gainsboro;
+    } */
+
+
     @media only screen and (max-width: 1024px) {
-        .right-menu{
-            visibility: hidden;
+        .right-menu, hr{
+            display: none;
         }
+
+        img{
+            content:url("/assets/medium-logo-sm.png");
+        }
+
     }
 </style>
